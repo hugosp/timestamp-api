bbe594233dd1e9a6da706f95b56ebe7982d6cd9d
hugosp <stefan.sundell@gmail.com>
2016-08-12 05:17:46 +0000

    there we go

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
+var moment = require('moment');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+//app.use(express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/',function(req, res) {
+    
 });
+
+
+app.get('/:time', function (req, res) {
+    
+    var time = req.params.time;
+    var html = {};
+    
+    var regex = /^[0-9]{10}/;
+    if(regex.test(time)) {
+        console.log('unix timestamp');
+        time *=1000;
+    }
+    
+    if(moment(time).isValid()) {
+        html.unix = moment(time).format('X');
+        html.natural = moment(time).format('MMMM Do, YYYY');
+    } else {
+        html.unix = 'null';
+        html.natural = 'null';
+    }
+    res.send(html);
+});
+
 
 app.listen(8080, function () {
     console.log('server up!');
